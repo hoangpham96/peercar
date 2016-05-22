4c5dc3476ec0e7840ce3091705eec46d3cf94612
smcm8974 <smcm8974@uni.sydney.edu.au>
2016-05-22 13:29:39 +1000

    Added .strip() to fix a bug. and some small formatting ajustments to ddl

--- DDL.sql
+++ DDL.sql
@@ -1,7 +1,6 @@
-﻿SELECT * FROM carbay;
-ALTER TABLE member ALTER password SET DATA TYPE CHAR(60)
-UPDATE member SET password ='$2b$12$e/BZXwFoEg9dHSlJ9uDQ..iexa5UWBXHnCXgEsqMIOGf01pmsSIju' WHERE memberno=1
-ALTER TABLE member DROP COLUMN pw_salt0
+﻿SET search_path TO carsharing;
+ALTER TABLE member ALTER password SET DATA TYPE CHAR(60);
+ALTER TABLE member DROP COLUMN pw_salt;
 
 --------------------------------------
 -- PROCEDURES --
